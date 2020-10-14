17bdffe27fdf270dc646385b3f663c70c40e4d8c
Surya <surya.sk05@outlook.com>
2020-10-14 15:41:52 +0530

    Setup

--- planets-opengl/planets.c
+++ planets-opengl/planets.c
@@ -1,1 +1,115 @@
+
+/************************************************************************************
+
+File: 			planets.c
+
+Description:	A complete OpenGL program that draws a solar system
+
+Author:			Surya Kashyap
+
+*************************************************************************************/
+
+//include libary header files
 #include <freeglut.h>
+
+//window dimensions
+GLint windowHeight = 600;
+GLint windowWidth = 600;
+
+
+
+/************************************************************************
+
+
+Function:		initializeGL
+
+
+Description:	Initializes the OpenGL rendering context for display.
+
+
+*************************************************************************/
+void initializeGL()
+{
+	// set the background color
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	// set default drawing color
+	glColor3f(1.0, 1.0, 1.0);
+
+	// set to projection mode 
+	glMatrixMode(GL_PROJECTION);
+
+	// load identity matrix
+	glLoadIdentity();
+
+	// set the shade model
+	glShadeModel(GL_SMOOTH);
+
+	// enable smooth line drawing
+	glEnable(GL_LINE_SMOOTH);
+
+	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+
+/************************************************************************
+
+
+Function:		myDisplay
+
+
+Description:	The display function
+
+
+*************************************************************************/
+
+void myDisplay()
+{
+	// clear the screen 
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPointSize(4.0);
+
+	glBegin(GL_POINTS);
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex2f(-0.5, 0.5);
+	glEnd();
+
+	// switch to the other buffer
+	glutSwapBuffers();
+}
+
+
+/************************************************************************
+
+Function:		main
+
+Description:	Sets up the openGL rendering context and the windowing
+
+system, then begins the display loop.
+
+
+*************************************************************************/
+void main(int argc, char** argv)
+{
+	//initialize toolkit
+	glutInit(&argc, argv);
+
+	// set display mode
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+	// set window size
+	glutInitWindowSize(windowWidth, windowHeight);
+
+	// open screen window
+	glutCreateWindow("Planets");
+
+	// set the display function
+	glutDisplayFunc(myDisplay);
+
+	// initialize the rendering context
+	initializeGL();
+
+	// go into perpetual loop
+	glutMainLoop();
+
+}
